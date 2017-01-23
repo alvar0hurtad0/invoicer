ba020dd45b1360954f14b93f654910c3de8df282
Alvaro J. Hurtado <alvaroh@aspgems.com>
2017-01-23 06:53:13 +0100

    Structure for calculate forms

--- modules/invoicer_invoice/js/InvoiceForm.js
+++ modules/invoicer_invoice/js/InvoiceForm.js
@@ -16,6 +16,25 @@
     Drupal.behaviors.invoicerInvoice = {
         attach: function (context) {
 
+            function getSiblingsValues ($element){
+                var $parent = jQuery($element.closest("td"));
+
+                var $values = {
+                    "quantity": $parent.find(".quantity").val(),
+                    // @todo: get values of all fields.
+                    // You may need to add some extra classes.
+                };
+
+                return $values;
+            }
+
+            $(".quantity").change(function() {
+                var $siblings = getSiblingsValues($(this));
+
+                // @todo: calculate Base price and total price.
+            });
+
+            //@todo: implement the amount.change, vat.change base-price.change and total price.change.
         }
     };
 

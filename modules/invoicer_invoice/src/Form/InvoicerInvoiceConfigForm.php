741d3ad178b04a79f235523b080abb4db8243707
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2017-01-24 18:43:25 +0100

    8.x-1.x: drupal standart format

--- modules/invoicer_invoice/src/Form/InvoicerInvoiceConfigForm.php
+++ modules/invoicer_invoice/src/Form/InvoicerInvoiceConfigForm.php
@@ -15,67 +15,68 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class InvoicerInvoiceConfigForm extends ConfigFormBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getFormId() {
-        return 'invoicer_invoice_config_form';
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'invoicer_invoice_config_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['invoicer_invoice.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('invoicer_invoice.settings');
+
+    $form = array();
+
+    $form['vat_types'] = array(
+      '#type' => 'textarea',
+      '#title' => t('VAT types'),
+      '#size' => 60,
+      '#maxlength' => 60,
+      '#default_value' => implode("\n", $config->get('vat_types')),
+      '#description' => t('The VAT types'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $config = $this->config('invoicer_invoice.settings');
+
+    $form_state->cleanValues();
+    foreach ($form_state->getValues() as $key => $value) {
+      if ($key == 'vat_types') {
+        $values = explode("\n", $value);
+
+        $values = array_map(function ($value) {
+
+          return trim($value);
+        }, $values);
+
+        $values = array_filter($values, function ($value) {
+          return (!empty($value)) && is_numeric($value);
+        });
+
+        $config->set($key, $values);
+      }
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getEditableConfigNames() {
-        return ['invoicer_invoice.settings'];
-    }
+    $config->save();
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        $config = $this->config('invoicer_invoice.settings');
-
-        $form = array();
-
-        $form['vat_types'] = array(
-            '#type' => 'textarea',
-            '#title' => t('VAT types'),
-            '#size' => 60,
-            '#maxlength' => 60,
-            '#default_value' => implode("\n", $config->get('vat_types')),
-            '#description' => t('The VAT types'),
-        );
-
-        return parent::buildForm($form, $form_state);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-
-        $config = $this->config('invoicer_invoice.settings');
-
-        $form_state->cleanValues();
-        foreach ($form_state->getValues() as $key => $value) {
-            if($key == 'vat_types') {
-                $values = explode("\n", $value);
-
-                $values = array_map(function ($value) {
-                   return trim($value);
-                }, $values);
-
-                $values = array_filter($values, function($value) {
-                    return (! empty($value)) && is_numeric($value);
-                });
-
-                $config->set($key, $values);
-            }
-        }
-
-        $config->save();
-
-        parent::submitForm($form, $form_state);
-    }
+    parent::submitForm($form, $form_state);
+  }
 
 }

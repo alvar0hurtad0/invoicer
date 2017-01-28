9a9f81a952acb8b8354fe0055c157c8260f947e8
Miguel Manzano García <mmanzano@gmail.com>
2017-01-28 23:23:51 +0100

    Get some work about javascript (#8)
    * 2845886: update with two decimals and listen to change in base_price and total_price
    
    * 2845886: some style fixing

--- modules/invoicer_invoice/js/InvoiceForm.js
+++ modules/invoicer_invoice/js/InvoiceForm.js
@@ -18,30 +18,49 @@
             $(".quantity").change(updateLinePrice);
             $(".amount").change(updateLinePrice);
             $(".vat").change(updateLinePrice);
+            $(".base_price").change(updateAmountAndTotalPrice);
+            $(".total_price").change(updateAmountAndBasePrice);
 
             function updateLinePrice() {
                 var $parent = jQuery($(this).closest("td"));
                 var $siblings = getSiblingsValues($parent);
 
-                $parent.find(".base_price").val($siblings["quantity"] * $siblings["amount"]);
-                $parent.find(".total_price").val($siblings["quantity"] * $siblings["amount"] * (1 + ($siblings["vat"] / 100)));
+                var $basePrice = $siblings["quantity"] * $siblings["amount"];
+                var $totalPrice = $basePrice * $siblings["vatFactor"];
+                $parent.find(".base_price").val(parseFloat($basePrice).toFixed(2));
+                $parent.find(".total_price").val(parseFloat($totalPrice).toFixed(2));
+            }
+
+            function updateAmountAndTotalPrice() {
+                var $parent = jQuery($(this).closest("td"));
+                var $siblings = getSiblingsValues($parent);
+
+                var $totalPrice = $siblings["base_price"] * $siblings["vatFactor"];
+                var $amount = $siblings["base_price"] / $siblings["quantity"];
+                $parent.find(".amount").val(parseFloat($amount).toFixed(2));
+                $parent.find(".total_price").val(parseFloat($totalPrice).toFixed(2));
+            }
+
+            function updateAmountAndBasePrice() {
+                var $parent = jQuery($(this).closest("td"));
+                var $siblings = getSiblingsValues($parent);
+
+                var $basePrice = $siblings["total_price"] / $siblings["vatFactor"];
+                var $amount = $basePrice / $siblings["quantity"];
+                $parent.find(".amount").val(parseFloat($amount).toFixed(2));
+                $parent.find(".base_price").val(parseFloat($basePrice).toFixed(2));
             }
 
             function getSiblingsValues($parent) {
                 return {
                     "quantity": $parent.find(".quantity").val(),
                     "amount": $parent.find(".amount").val(),
-                    "vat": $parent.find(".vat").val()
+                    "vat": $parent.find(".vat").val(),
+                    "vatFactor": 1 + ($parent.find(".vat").val() / 100),
+                    "base_price": $parent.find(".base_price").val(),
+                    "total_price": $parent.find(".total_price").val()
                 };
             }
-
-            $(".quantity").change(function () {
-                var $siblings = getSiblingsValues($(this));
-
-                // @todo: calculate Base price and total price.
-            });
-
-            // @todo: implement the amount.change, vat.change base-price.change and total price.change.
         }
     };
 
